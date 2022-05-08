0d5c3454b182d200cdecdca88a67c359b7f7bf90
unknown <islam15-3879@diu.edu.bd>
2022-05-08 23:13:49 +0600

    extra-added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { ObjectID } = require('bson');
 
 require('dotenv').config();
 const app=express();
-const port =process.env.port||5000;
+const port =process.env.PORT||5000;
 
 //MIDDEL WIRE
 app.use(cors());
